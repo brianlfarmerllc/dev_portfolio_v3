8e332aed812526fa08392297fa54f8149fe12ab4
Brian Farmer <brianlfarmerllc@gmail.com>
2021-06-20 22:28:13 -0500

    auto hiding header on scroll working all transitions to anchors on page looks good

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,6 +1,30 @@
+import { useState } from "react";
+import useDocumentScrollThrottled from "../documentScroll";
+
 function Header({ header }) {
+  const [shouldHideHeader, setShouldHideHeader] = useState(false);
+  const [shouldShowShadow, setShouldShowShadow] = useState(false);
+
+  const MINIMUM_SCROLL = 80;
+  const TIMEOUT_DELAY = 100;
+
+  useDocumentScrollThrottled((callbackData) => {
+    const { previousScrollTop, currentScrollTop } = callbackData;
+    const isScrolledDown = previousScrollTop < currentScrollTop;
+    const isMinimumScrolled = currentScrollTop > MINIMUM_SCROLL;
+
+    setShouldShowShadow(currentScrollTop > 2);
+
+    setTimeout(() => {
+      setShouldHideHeader(isScrolledDown && isMinimumScrolled);
+    }, TIMEOUT_DELAY);
+  });
+
+  const shadowStyle = shouldShowShadow ? "shadow" : "";
+  const hiddenStyle = shouldHideHeader ? "hidden" : "";
+
   return (
-    <header className={`navHeader`}>
+    <header className={`navHeader ${shadowStyle} ${hiddenStyle}`}>
       <nav className={header}>
         <a href="#hero" className="logo">
           <div className="diamond">
